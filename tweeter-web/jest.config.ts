9a1606b9688979b45aea6c177dcde4d84db44c51
ammonwk <ammonwk@gmail.com>
2026-04-02 01:52:17 -0600

    installed ts-mockito

---
+++ tweeter-web/jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterSetup: ['<rootDir>/src/setupTests.ts'],
+  moduleNameMapper: {
+    "\\.(css|scss)$": "<rootDir>/src/__mocks__/styleMock.ts",
+    "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/src/__mocks__/fileMock.ts",
+  },
+};
